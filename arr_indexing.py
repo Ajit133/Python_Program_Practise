5c90cfdc35848a10ebbd716f55b3a0c18af643db
Ajit133 <dasajit697@gmail.com>
2022-01-26 20:41:05 +0600

    Slice in Array

--- arr_indexing.py
+++ arr_indexing.py
@@ -1,3 +1,6 @@
 country = ["Bhutan","Nepal","France","Italy","Brazil","Germany","USA"]
-new_list = country[1:5]
+new_list = country[2::2] #Upto
 print(new_list)
+country1 = ["Bhutan","Nepal","France","Italy","Brazil","Germany","USA"]
+new_list1 = country[-5:-1:]
+print(new_list1)
